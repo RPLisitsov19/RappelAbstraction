86ded99b827deb7fe5112e52152296353c135963
BDSimeonov19 <BDSimeonov19@codingburgas.bg>
2021-04-26 21:03:12 +0300

    Add personnel

--- Project/Js/app.js
+++ Project/Js/app.js
@@ -1,26 +1,56 @@
+function personnel(display) {
+    let lastNames = [
+    "Pagan","Land","Tidwell",
+    "Lovell","Miner","Doss",
+    "Dahl","Delatorre","Stanford",
+    "Kauffman","Vela","Gagnon",
+    "Winston","Gomes","Thacker",
+    "Coronado","Ash","Jarrett",
+    "Hager","Samuels","Metzger",
+    "Raines","Spivey","Maurer",
+    "Han","Voss","Henley",
+    "Caballero","Caruso","Coulter",
+    "North","Finn","Cahill",
+    "Lanier","Souza","Mcwilliams"];
+
+    let chosenName = lastNames[Math.floor(36 * Math.random())];
+
+    return chosenName; 
+    
+}
+
+var info = {};
+
 function submitForm(){
     let display = document.getElementById("paragraph");
-    display.innerHTML = "Your support ticket";
 
-    display.innerHTML += "<br>";
+    info.nameVal = document.getElementById("fname").value;
+    info.addressVal = document.getElementById("address").value;
+    info.coords = document.getElementById("coords").value;
 
-    let nameVal = document.getElementById("fname").value;
-    display.innerHTML += nameVal + "<br>";
 
-    let addressVal =  document.getElementById("address").value;
-    display.innerHTML += addressVal + "<br>";
 
-    let coords = document.getElementById("coords").value;
-    display.innerHTML += coords + "<br>";
+    info.accident = document.getElementsByName("accident");
 
-    let accident = document.getElementsByName("accident");
     
-    for(let i = 0; i < accident.length; i++){
-        if(accident[i].checked){
-            display.innerHTML += accident[i].value + "<br>";
+
+    display.innerHTML = 'Your support ticket<br>';
+    display.innerHTML += `Full name: ${info.nameVal}<br>`;
+    display.innerHTML += `Address: ${info.addressVal}<br>`;
+    display.innerHTML += `Location coordinates: ${info.coords}<br>`;
+
+    for(let i = 0; i < info.accident.length; i++){
+        if(info.accident[i].checked){
+            display.innerHTML += `${info.accident[i].value}<br>`;
             break;
         }
-    }
+    }   
+
+
+    info.officer = `<br>Responsible for the operation is officer ${personnel(display)}`;
+    display.innerHTML += info.officer;
+
 
     return false;
+
 }
